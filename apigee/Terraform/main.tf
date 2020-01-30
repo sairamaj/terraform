619bcf233ed25ac74aa82d3fbe423f9228c2f06f
Sairama <sairamaj@gmail.com>
2020-01-29 23:31:37 -0800

    Working API Proxy

--- apigee/Terraform/main.tf
+++ apigee/Terraform/main.tf
@@ -5,29 +5,29 @@ provider "apigee" {
   
 }
 
-# data "archive_file" "bundle" {
-#    type         = "zip"
-#    source_dir   = "${path.module}/proxy_files"
-#    output_path  = "${path.module}/proxy_files_bundle/apiproxy.zip"
-# }
-# resource "apigee_api_proxy" "helloworld_proxy" {
-#    name  = "helloworld2"                         # The proxy name.
-#    bundle       = "${data.archive_file.bundle.output_path}" # Apigee APIs require a zip bundle to import a proxy.
-#    bundle_sha   = "${data.archive_file.bundle.output_sha}"  # The SHA is used to detect changes for plan/apply.   
-# }
-
-resource "apigee_developer" "helloworld_developer" {
-   email = "helloworld_email@test.com"                                  # required
-   first_name = "helloworld"                                            # required
-   last_name = "thelloworld1"                                           # required
-   user_name = "helloworld1"                                            # required
-
-   attributes = {                                                         # optional
-      DisplayName = "my_awesome_app_updated"
-      Notes = "notes_for_developer_app_updated"
-	  custom_attribute_name = "custom_attribute_value"
-   }
+data "archive_file" "bundle" {
+   type         = "zip"
+   source_dir   = "${path.module}/proxy_files"
+   output_path  = "${path.module}/proxy_files_bundle/apiproxy.zip"
 }
+resource "apigee_api_proxy" "helloworld_proxy" {
+   name  = "helloworld2"                         # The proxy name.
+   bundle       = "${data.archive_file.bundle.output_path}" # Apigee APIs require a zip bundle to import a proxy.
+   bundle_sha   = "${data.archive_file.bundle.output_sha}"  # The SHA is used to detect changes for plan/apply.   
+}
+
+# resource "apigee_developer" "helloworld_developer" {
+#    email = "helloworld2_email@test.com"                                  # required
+#    first_name = "helloworld2"                                            # required
+#    last_name = "thelloworld2"                                           # required
+#    user_name = "helloworld2"                                            # required
+
+#    attributes = {                                                         # optional
+#       DisplayName = "my_awesome_app_updated"
+#       Notes = "notes_for_developer_app_updated"
+# 	  custom_attribute_name = "custom_attribute_value"
+#    }
+# }
 
 # A company
 # resource "apigee_company" "helloworld_company" {
